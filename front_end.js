3eed0c207fcf398f49aa37e6e4dd0432dc3db410
Olgerd_Seviakov <olgerd@zenitech.co.uk>
2020-05-14 15:54:02 +0300

    transfer some how

--- front_end.js
+++ front_end.js
@@ -1,10 +1,36 @@
 const apiServerBaseUrl = 'http://localhost:5000'
 const proxiedApiServerBaseUrl = 'http://localhost:8000'
-const helloUrl = proxiedApiServerBaseUrl+'/hello'
+const helloUrl = proxiedApiServerBaseUrl + '/hello'
 
 const sendRequest = async () => {
   const options = { headers: { accept: 'application/json' } }
   const response = await fetch(helloUrl, options)
   const data = await response.json()
   document.getElementById('response-data').textContent = JSON.stringify(data)
+}
+
+const images = [
+  'https://cdn.eso.org/images/thumb300y/eso1907a.jpg',
+  'https://api.media.atlassian.com/file/90edef1f-f222-44e7-8a58-e60e4b70826e/binary?token=eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI0ZTlmNGFiMS1iN2FiLTQ5YjItODlkOS05ZmEyMDc2NDI0ZTMiLCJhY2Nlc3MiOnsidXJuOmZpbGVzdG9yZTpmaWxlOjkwZWRlZjFmLWYyMjItNDRlNy04YTU4LWU2MGU0YjcwODI2ZSI6WyJyZWFkIl19LCJleHAiOjE1ODk1MzczNDUsIm5iZiI6MTU4OTQ1NDM2NX0.l-bBjqc3lgq96S7fC1PMmFksac1mBOb4QqaJot5U4aU&client=4e9f4ab1-b7ab-49b2-89d9-9fa2076424e3&name=image.png',
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Square_200x200.svg/1200px-Square_200x200.svg.png'
+]
+
+const getImageData = async (imageUrl) => {
+  const options = { headers: { accept: 'image/*' } }
+  const response = await fetch(`http://localhost:8000/image/${encodeURIComponent(imageUrl)}`, options)
+  return await response.text()
+}
+const appendToContainer = (url) => {
+  const elem = document.createElement("img");
+  elem.setAttribute("src", url);
+  elem.setAttribute("height", "200");
+  elem.setAttribute("width", "200");
+  elem.setAttribute("alt", "loaded image");
+  document.getElementById("container").appendChild(elem);
+}
+
+window.onload = async () => {
+  for (const imageUrl of images) {
+    appendToContainer(await getImageData(imageUrl))
+  }
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const http = require('http')
 const httpProxy = require('http-proxy')
+const fetch = require('node-fetch');
 const app = express()
 
 const proxy = httpProxy.createProxyServer({});
@@ -17,7 +18,7 @@ proxy.on('error', function (err, req, res) {
   res.writeHead(500, {
     'Content-Type': 'text/plain'
   });
-  res.end("Oops");
+  res.end(err.message);
 });
 
 proxy.on('proxyRes', function(proxyRes, req, res) {
@@ -29,6 +30,19 @@ proxy.on('proxyRes', function(proxyRes, req, res) {
 
 app.get('/hello', (req, res) => {
   return res.json({ text: 'Hello World!' });
+})
+
+app.get('/image/:imageUrl', async (req, res) => {
+  const options = {
+    method: 'get',
+    headers: { 'accept': 'image/*' },
+  }
+  const { imageUrl } = req.params
+  const response = await fetch(decodeURIComponent(imageUrl), options);
+  const type = response.headers.get('content-type')
+  const buffer = await response.buffer()
+  const base64 = buffer.toString('base64')
+  res.send('data:'+type+';base64,'+base64)
 })
 const apiServerPort = 5000
 app.listen(apiServerPort, () => console.log(`Api server on http://localhost:${apiServerPort}`))
